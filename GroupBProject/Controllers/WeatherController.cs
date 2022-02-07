27c9959f44fd3c8c89b1a18dfd9ecd78a86afc7b
Ashiq Nazar <khanashiq18@gmail.com>
2022-02-07 16:08:48 +0530

    Modified Weather ApiAdd project files.

---
+++ GroupBProject/Controllers/WeatherController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GroupBProject.Controllers
+{
+    [Route("api/Controller")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        // GET: api/<WeatherController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WeatherController>/5
+        [HttpGet]
+        [Route("GetWeather")]
+        public string GetWeather()
+        {
+       
+            var client = new RestClient("http://api.weatherapi.com/v1/current.json?key=d90506ffda3441ba9eb41606220702&q=London&aqi=no");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = (IRestResponse)client.Execute(request);
+            Console.WriteLine(response.Content);
+            return response.Content;
+        }
+
+        // POST api/<WeatherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WeatherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WeatherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GroupBProject/Model/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupBProject.Model
+{
+    public class Employee
+    {
+        public string Name  { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
